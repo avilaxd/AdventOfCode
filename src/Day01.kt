258b44b20402e1e92284e3705481554d8f746421
Armando Ávila Bueno <ppicucha@hotmail.com>
2023-12-06 18:03:21 -0600

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,68 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        for (line in input) {
+            val digits = line.filter { it.isDigit() }
+            val number = "${digits.first()}${digits.last()}".toIntOrNull()
+            number?.let { sum += number }
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val replacedList = mutableListOf<String>()
+        input.forEach {
+            val overlappingIndexes = it.getOverlappingIndexes()
+            var line = it
+            overlappingIndexes.forEachIndexed { index, value ->
+                val letter = line[value]
+                 line = line.substring(0, value + index) + letter + line.substring(value + index)
+            }
+            replacedList.add(line.replaceSpelledDigits())
+        }
+        return part1(replacedList)
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+fun getSpelledDigitsMap(): Map<String, String> {
+    return mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9"
+    )
+}
+
+fun String.getOverlappingIndexes(): List<Int> {
+    val indexes = mutableListOf<Int>()
+    getSpelledDigitsMap().forEach { mapEntry ->
+        val firstIndexes = Regex(mapEntry.key).findAll(this)
+        .map { appearance -> appearance.range.first }
+        .toList()
+        firstIndexes.forEach {
+            indexes.add(it)
+            indexes.add(it + (mapEntry.key.length - 1))
+        }
+    }
+    return findDuplicates(indexes)
+}
+
+fun findDuplicates(items: List<Int>): List<Int> {
+    return items.filter { item -> items.count { it == item } > 1 }.toSet().toList()
+}
+
+fun String.replaceSpelledDigits(): String {
+    var finalString = this
+    getSpelledDigitsMap().forEach { finalString = finalString.replace(it.key, it.value) }
+    return finalString
 }
